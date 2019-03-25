c021463ed1f078e3fe11969e6d1292ef7e1cae79
Alistair Bill <alistair.bill@gmail.com>
2019-03-25 11:07:29 +0000

    Use an array for stat names

--- src/devices/fsl_scoreboard.c
+++ src/devices/fsl_scoreboard.c
@@ -3,31 +3,26 @@
 
 #include "decoder.h"
 
-char *fsl_stat_name(uint8_t command) {
-    char *out = "0xxx";
-    switch(command) {
-        case 0x0: return "brightness"; break;
-        case 0x1: return "lh_no";      break;
-        case 0x2: return "rh_no";      break;
-        case 0x3: return "lh_score";   break;
-        case 0x4: return "rh_score";   break;
-        case 0x5: return "total";      break;
-        case 0x6: return "wickets";    break;
-        case 0x7: return "overs";      break;
-        case 0x8: return "extras";     break;
-        case 0x9: return "last_man";   break;
-        case 0xa: return "last_wkt";   break;
-        case 0xb: return "1st_inns";   break;
-        case 0xc: return "runs_rqd";   break;
-        case 0xd: return "ovs_left";   break;
-        case 0xe: return "bowler_1";   break;
-        case 0xf: return "bowler_2";   break;
-    }
-    sprintf(out, "0x%x", command);
-    return out;
-}
+static char *fsl_stat_name[] = {
+    "brightness",
+    "lh_no",
+    "rh_no",
+    "lh_score",
+    "rh_score",
+    "total",
+    "wickets",
+    "overs",
+    "extras",
+    "last_man",
+    "last_wkt",
+    "1st_inns",
+    "runs_rqd",
+    "ovs_left",
+    "bowler_1",
+    "bowler_2",
+};
 
-inline int fsl_digit(uint8_t digit) {
+static inline int fsl_digit(uint8_t digit) {
     /* Digits which should not be displayed have a value of 0xf.
      * Convert them to 0.
      */
@@ -57,11 +52,11 @@ static int fsl_scoreboard_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
     uint8_t hundreds = b[1] & 0xf;
     uint8_t tens = b[2] & 0xf;
     uint8_t units = b[3] & 0xf;
-    uint8_t parity = b[4] >> 7;
+    uint8_t parity = b[4] >> 7; // TODO: figure out checksum
     uint16_t score_value = fsl_digit(hundreds) * 100 + fsl_digit(tens) * 10 + fsl_digit(units);
 
     data = data_make("model", "", DATA_STRING, "FSL Scoreboard Remote",
-                     "statistic", "Statistic", DATA_STRING, fsl_stat_name(stat),
+                     "statistic", "Statistic", DATA_STRING, fsl_stat_name[stat],
                      "hundreds", "Hundreds digit", DATA_INT, hundreds,
                      "tens", "Tens digit", DATA_INT, tens,
                      "units", "Units digit", DATA_INT, units,
